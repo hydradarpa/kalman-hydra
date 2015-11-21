73fb540da200d567cea8a0c87385e426b79f5a4f
lansdell <lansdell@uw.edu>
2015-11-20 17:47:22 -0800

    testmeshgen 'homography'

---
+++ distmesh_dyn.py
@@ -0,0 +1,243 @@
+import numpy as np
+import cv2
+import distmesh as dm 
+import scipy.spatial as spspatial
+import distmesh.mlcompat as ml
+import distmesh.utils as dmutils
+
+from imgproc import * 
+
+class DistMesh:
+	def __init__(self, frame_orig, h0 = 25, dptol = 0.01):
+		self.dptol = dptol
+		self.h0 = h0
+
+		self.fh = dm.huniform; 
+		nx,ny = np.shape(frame_orig)[0:2]
+		self.nx = nx
+		self.ny = ny
+		self.bbox = (0, 0, ny, nx)
+		self.ttol=.1
+		self.Fscale=1.2
+		self.deltat=.2
+		self.geps=.001*h0;
+		self.deps=np.sqrt(np.finfo(np.double).eps)*h0;
+		self.densityctrlfreq = 30;
+		self.k = 1
+
+	def createMesh(self, ctrs, fd, frame_orig):
+		pfix = None 
+	
+		# Extract bounding box
+		xmin, ymin, xmax, ymax = self.bbox
+		if pfix is not None:
+			pfix = np.array(pfix, dtype='d')
+		
+		#1. Set up initial points 
+		x, y = np.mgrid[xmin:(xmax+self.h0):self.h0,
+										ymin:(ymax+self.h0*np.sqrt(3)/2):self.h0*np.sqrt(3)/2]
+		x[:, 1::2] += self.h0/2                               # Shift even rows
+		p = np.vstack((x.flat, y.flat)).T                # List of node coordinates
+		
+		# 2. Remove points outside the region, apply the rejection method
+		a = fd(p)
+		p = p[np.where(a<self.geps)]                          # Keep only d<0 points
+		r0 = 1/self.fh(p)**2                                  # Probability to keep point
+		p = p[np.random.random(p.shape[0])<r0/r0.max()]  # Rejection method
+		if pfix is not None:
+			p = ml.setdiff_rows(p, pfix)                 # Remove duplicated nodes
+			pfix = ml.unique_rows(pfix); nfix = pfix.shape[0]
+			p = np.vstack((pfix, p))                     # Prepend fix points
+		else:
+			nfix = 0
+		N = p.shape[0]                                   # Number of points N
+		
+		count = 0
+		pold = float('inf')                              # For first iteration
+		
+		################################################################################
+		#Mesh creation
+		################################################################################
+		
+		while True:
+			count += 1
+			# 3. Retriangulation by the Delaunay algorithm
+			dist = lambda p1, p2: np.sqrt(((p1-p2)**2).sum(1))
+			if (dist(p, pold)/self.h0).max() > self.ttol:          # Any large movement?
+				pold = p.copy()                          # Save current positions
+				self.delaunay = spspatial.Delaunay(p)
+				t = self.delaunay.vertices       # List of triangles
+				pmid = p[t].sum(1)/3                     # Compute centroids
+				t = t[fd(pmid) < -self.geps]                  # Keep interior triangles
+				# 4. Describe each bar by a unique pair of nodes
+				bars = np.vstack((t[:, [0,1]],
+									t[:, [1,2]],
+									t[:, [2,0]]))          # Interior bars duplicated
+				bars.sort(axis=1)
+				bars = ml.unique_rows(bars)              # Bars as node pairs
+				#Plot:
+				if frame_orig is not None:
+					frame = frame_orig.copy()
+					#drawPoints(frame, p)
+					drawGrid(frame, p, bars)
+					cv2.imshow('frame',frame)
+					k = cv2.waitKey(30) & 0xff
+					if k == 27:
+						break
+		
+			# 6. Move mesh points based on bar lengths L and forces F
+			barvec = p[bars[:,0]] - p[bars[:,1]]         # List of bar vectors
+			L = np.sqrt((barvec**2).sum(1))              # L = Bar lengths
+			hbars = self.fh(p[bars].sum(1)/2)
+			L0 = 1.4*self.h0*np.ones_like(L); #(hbars*Fscale
+						#*np.sqrt((L**2).sum()/(hbars**2).sum()))  # L0 = Desired lengths
+		
+			F = self.k*(L0-L)
+			F[F<0] = 0#F[F<0]*.5#0                         # Bar forces (scalars)
+			Fvec = F[:,None]/L[:,None].dot([[1,1]])*barvec # Bar forces (x,y components)
+			Ftot = ml.dense(bars[:,[0,0,1,1]],
+											np.repeat([[0,1,0,1]], len(F), axis=0),
+											np.hstack((Fvec, -Fvec)),
+											shape=(N, 2))
+			Ftot[:nfix] = 0                              # Force = 0 at fixed points
+			p += self.deltat*Ftot                             # Update node positions
+		
+			# 7. Bring outside points back to the boundary
+			d = fd(p); ix = d>0                          # Find points outside (d>0)
+			ddeps = 1e-1
+			for idx in range(10):
+				if ix.any():
+					dgradx = (fd(p[ix]+[ddeps,0])-d[ix])/ddeps # Numerical
+					dgrady = (fd(p[ix]+[0,ddeps])-d[ix])/ddeps # gradient
+					dgrad2 = dgradx**2 + dgrady**2
+					p[ix] -= (d[ix]*np.vstack((dgradx, dgrady))/dgrad2).T # Project
+		
+			# 8. Termination criterion: All interior nodes move less than dptol (scaled)
+			if (np.sqrt((self.deltat*Ftot[d<-self.geps]**2).sum(1))/self.h0).max() < self.dptol:
+				break
+		
+		self.p = p 
+		self.t = t 
+		self.bars = bars
+		self.L = L 
+
+	def updateMesh(self, ctrs, fd, frame_orig, pfix = None, n_iter = 2):
+	
+		xmin, ymin, xmax, ymax = self.bbox
+		
+		if pfix is not None:
+			self.p = ml.setdiff_rows(self.p, pfix)
+			pfix = ml.unique_rows(pfix)
+			nfix = pfix.shape[0]
+		else:
+			nfix = 0
+	
+		N = self.p.shape[0]                                   # Number of points N
+		pold = float('inf')                              # For first iteration
+	
+		################################################################################
+		#Mesh updates
+		################################################################################
+		
+		self.delaunay = spspatial.Delaunay(self.p)
+		self.t = self.delaunay.vertices       # List of triangles
+		for ii in range(n_iter):
+			dist = lambda p1, p2: np.sqrt(((p1-p2)**2).sum(1))
+			if (dist(self.p, pold)/self.h0).max() > self.ttol:          # Any large movement?
+				pold = self.p.copy()                          # Save current positions
+				pmid = self.p[self.t].sum(1)/3                     # Compute centroids
+				self.t = self.t[fd(pmid) < -self.geps]                  # Keep interior triangles
+	
+				bars = np.vstack((self.t[:, [0,1]],
+									self.t[:, [1,2]],
+									self.t[:, [2,0]]))          # Interior bars duplicated
+				bars.sort(axis=1)
+				bars = ml.unique_rows(bars)              # Bars as node pairs
+	
+			barvec = self.p[bars[:,0]] - self.p[bars[:,1]]         # List of bar vectors
+			L = np.sqrt((barvec**2).sum(1))              # L = Bar lengths
+			hbars = self.fh(self.p[bars].sum(1)/2)
+			L0 = 5*self.h0*np.ones_like(L);
+		
+			F = self.k*(L0-L)
+			F[F<0] = 0                         # Bar forces (scalars)
+			Fvec = F[:,None]/L[:,None].dot([[1,1]])*barvec # Bar forces (x,y components)
+			Ftot = ml.dense(bars[:,[0,0,1,1]],
+											np.repeat([[0,1,0,1]], len(F), axis=0),
+											np.hstack((Fvec, -Fvec)),
+											shape=(N, 2))
+			Ftot[:nfix] = 0                              # Force = 0 at fixed points
+			self.p += self.deltat*Ftot                             # Update node positions
+		
+			d = fd(self.p); ix = d>0                          # Find points outside (d>0)
+			ddeps = 1e-1
+			for idx in range(10):
+				if ix.any():
+					dgradx = (fd(self.p[ix]+[ddeps,0])-d[ix])/ddeps # Numerical
+					dgrady = (fd(self.p[ix]+[0,ddeps])-d[ix])/ddeps # gradient
+					dgrad2 = dgradx**2 + dgrady**2
+					self.p[ix] -= (d[ix]*np.vstack((dgradx, dgrady))/dgrad2).T # Project	
+
+		self.bars = bars
+		self.L = L 
+
+
+#	def updateMesh(self, ctrs, fd, frame_orig, pfix = None, n_iter = 4):
+#	
+#		xmin, ymin, xmax, ymax = self.bbox
+#		
+#		if pfix is not None:
+#			self.p = ml.setdiff_rows(self.p, pfix)
+#			pfix = ml.unique_rows(pfix)
+#			nfix = pfix.shape[0]
+#		else:
+#			nfix = 0
+#	
+#		N = self.p.shape[0]                                   # Number of points N
+#		pold = float('inf')                              # For first iteration
+#	
+#		################################################################################
+#		#Mesh updates
+#		################################################################################
+#		
+#		for ii in range(n_iter):
+#			dist = lambda p1, p2: np.sqrt(((p1-p2)**2).sum(1))
+#			if (dist(self.p, pold)/self.h0).max() > self.ttol:          # Any large movement?
+#				pold = self.p.copy()                          # Save current positions
+#				self.delaunay = spspatial.Delaunay(self.p)
+#				self.t = self.delaunay.vertices       # List of triangles
+#				pmid = self.p[self.t].sum(1)/3                     # Compute centroids
+#				self.t = self.t[fd(pmid) < -self.geps]                  # Keep interior triangles
+#	
+#				bars = np.vstack((self.t[:, [0,1]],
+#									self.t[:, [1,2]],
+#									self.t[:, [2,0]]))          # Interior bars duplicated
+#				bars.sort(axis=1)
+#				bars = ml.unique_rows(bars)              # Bars as node pairs
+#	
+#			barvec = self.p[bars[:,0]] - self.p[bars[:,1]]         # List of bar vectors
+#			L = np.sqrt((barvec**2).sum(1))              # L = Bar lengths
+#			hbars = self.fh(self.p[bars].sum(1)/2)
+#			L0 = 1.4*self.h0*np.ones_like(L);
+#		
+#			F = L0-L
+#			F[F<0] = 0                         # Bar forces (scalars)
+#			Fvec = F[:,None]/L[:,None].dot([[1,1]])*barvec # Bar forces (x,y components)
+#			Ftot = ml.dense(bars[:,[0,0,1,1]],
+#											np.repeat([[0,1,0,1]], len(F), axis=0),
+#											np.hstack((Fvec, -Fvec)),
+#											shape=(N, 2))
+#			Ftot[:nfix] = 0                              # Force = 0 at fixed points
+#			self.p += deltat*Ftot                             # Update node positions
+#		
+#			d = fd(p); ix = d>0                          # Find points outside (d>0)
+#			ddeps = 1e-1
+#			for idx in range(10):
+#				if ix.any():
+#					dgradx = (fd(p[ix]+[ddeps,0])-d[ix])/ddeps # Numerical
+#					dgrady = (fd(p[ix]+[0,ddeps])-d[ix])/ddeps # gradient
+#					dgrad2 = dgradx**2 + dgrady**2
+#					p[ix] -= (d[ix]*np.vstack((dgradx, dgrady))/dgrad2).T # Project	
+#
+#		self.bars = bars
+#		self.L = L 
---
+++ testfun/testmeshgen_homography.py
@@ -0,0 +1,145 @@
+import numpy as np
+import cv2
+import scipy.spatial as spspatial
+import distmesh as dm 
+import distmesh.mlcompat as ml
+import distmesh.utils as dmutils
+import time 
+from matplotlib import pyplot as plt
+
+from imgproc import * 
+from distmesh_dyn import *
+
+################################################################################
+#Parameters
+################################################################################
+
+threshold = 7
+fn = "./video/local_prop_cb_with_bud.avi"
+fn_out = './video/local_pro_cb_with_bud_sparseflow_grid.avi'
+lk_params = dict( winSize  = (19, 19),
+				  maxLevel = 2,
+				  criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+feature_params = dict( maxCorners = 1000,
+					   qualityLevel = 0.01,
+					   minDistance = 8,
+					   blockSize = 19 )
+
+################################################################################
+#Set up object
+################################################################################
+
+cap = cv2.VideoCapture(fn)
+ret, frame = cap.read()
+frame_orig = frame.copy()
+(mask, ctrs, fd) = findObjectThreshold(frame, threshold = threshold)
+ofLK = OpticalFlowLK(cap, lk_params, feature_params)
+
+nx,ny = frame.shape[0:2]
+
+#Show the outline
+cv2.drawContours(frame, ctrs.contours, -1, (0,255,0), 1)
+cv2.imshow('frame',frame)
+k = cv2.waitKey(30) & 0xff
+
+fps = 20.0
+framesize = np.shape(frame)[0:2]
+framesize = framesize[::-1]
+fourcc = cv2.VideoWriter_fourcc(*'MP4V') 
+output = cv2.VideoWriter(fn_out,fourcc, fps, framesize)
+
+################################################################################
+#Mesh creation
+################################################################################
+
+distmesh = DistMesh(frame)
+distmesh.createMesh(ctrs, fd, frame)
+
+################################################################################
+#Main loop
+################################################################################
+
+prvs = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+#opticflowtime = 0
+#findsimplextime = 0 
+#findobjecttime = 0 
+#updatemeshtime = 0 
+#drawingtime = 0 
+
+while(cap.isOpened()):
+	ret, frame2 = cap.read()
+	if ret == False:
+		break 
+	next = cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
+
+	#Interpolate from sparse optic flow, using findHomography example:
+	#Get sparse optic flow of a set of points with each frame
+
+	#oft = time.time() 
+	flowimg = ofLK.run(next, prvs, frame2)
+	p0LK = ofLK.p0 
+	p1LK = ofLK.p1
+	flow = p1LK - p0LK
+	#opticflowtime += time.time() - oft 
+
+	#fst = time.time() 
+	##Use these points to shift around adjacent points:
+	nmvp = np.zeros((np.shape(distmesh.p)[0],1))
+	mvp = np.zeros((np.shape(distmesh.p)))
+	#Only look at points that are within boundary
+	fdp1 = fd(p1LK[:,0,:])
+	inbdry = fdp1 < 0 
+
+	#Find points adjacent to each tracked point
+	for tpt, tflow in zip(p0LK[inbdry,0,:], flow[inbdry,0,:]):
+		simpidx = distmesh.delaunay.find_simplex(tpt)
+		simp = distmesh.delaunay.vertices[simpidx]
+		nmvp[simp] += 1 
+		mvp[simp] += tflow
+
+	#For each point, take the average motion (if adjacent to more than one
+	#tracked point)
+	moved = nmvp > 0 
+	mvp[moved[:,0]] = np.divide(mvp[moved[:,0]], np.hstack((nmvp[moved,np.newaxis], nmvp[moved,np.newaxis])))
+	distmesh.p += mvp
+
+	#findsimplextime += time.time() - fst 
+
+
+	#umt = time.time() 
+	#Update the other points
+	###Recompute contours
+	#fot = time.time()
+	(mask, ctrs, fd) = findObjectThreshold(frame2, threshold = threshold)
+	#findobjecttime += time.time() - fot 
+	##-Reset bar lengths for points that moved from flow.
+	##-For points that didn't move, move these points with their forces,
+	## keeping the points that did move from flow fixed.
+	##-Add a few iterations of motion for all points to stabilize noisy movements
+	#umt = time.time() 
+	distmesh.updateMesh(ctrs, fd, next)
+	#updateMesh(distmesh, ctrs, fd, next)
+	#updatemeshtime += time.time() - umt 
+
+	#Draw contour and lines
+	#dt = time.time()
+	cv2.drawContours(frame2, ctrs.contours, -1, (0,255,0), 1)
+	drawGrid(frame2, distmesh.p, distmesh.bars)
+	cv2.imshow('frame',frame2)
+	k = cv2.waitKey(30) & 0xff
+	if k == 27:
+		break
+	output.write(frame2)
+	#drawingtime += time.time() - dt
+
+	prvs = next
+
+	#totaltime = drawingtime + updatemeshtime + findsimplextime + opticflowtime
+	#totaltime = updatemeshtime + findobjecttime
+	#print 'Drawing', 100*drawingtime/totaltime, 'Update Mesh', 100*updatemeshtime/totaltime, 'Find simplex', 100*findsimplextime/totaltime, 'Optic flow', 100*opticflowtime/totaltime
+	#print 'Update Mesh', 100*updatemeshtime/totaltime, 'Find object', 100*findobjecttime/totaltime
+
+cap.release()
+output.release()
+cv2.destroyAllWindows()
