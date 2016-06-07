fd31df3027fd8d9422bc4951fcb5a57df3aed985
Ben Lansdell <lansdell@uw.edu>
2016-06-06 19:12:44 -0700

    Update flowfields

--- synthetic/flowfields.py
+++ synthetic/flowfields.py
@@ -1,8 +1,8 @@
 flowfields = {}
 #Velocity fields:
-flowfields['translate_leftup'] = lambda pt,t: (-3., -3.)
-flowfields['translate_leftup_stretch'] = lambda pt,t: (-3.+pt[0]/200., -3.+pt[1]/200.)
-y0 = 500.
-x0 = 500.
-flowfields['rotate'] = lambda pt,t: (-(pt[1]-y0)/100., (pt[0]-x0)/100.)
-flowfields['warp'] = lambda pt,t: (-(pt[0]/500.-1.)*pt[0]*(pt[0]/1000.-1.)/100.,(pt[1]/500.-1.)*pt[1]*(pt[1]/1000.-1.)/100.)
+flowfields['translate_leftup'] = lambda pt,t: (-1.5., -1.5.)
+flowfields['translate_leftup_stretch'] = lambda pt,t: (-1.+pt[0]/200., -1.+pt[1]/200.)
+y0 = 300.
+x0 = 300.
+flowfields['rotate'] = lambda pt,t: (-(pt[1]-y0)/25., (pt[0]-x0)/25.)
+flowfields['warp'] = lambda pt,t: (-(pt[0]/300.-1.)*pt[0]*(pt[0]/600.-1.)/75.,(pt[1]/300.-1.)*pt[1]*(pt[1]/600.-1.)/75.)
